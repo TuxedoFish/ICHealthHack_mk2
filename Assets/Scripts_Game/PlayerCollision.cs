ce593fc7bd840294534b3af40ca8bd276362e3c1
Rhiannon Dowling <r_dowling@live.co.uk>
2018-03-17 19:19:03 +0000

    Add all files, initial commit

---
+++ Assets/Scripts_Game/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement; //reference playerMovement script
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {//check object we cllided with has tag obstacle
+        if (collisionInfo.collider.tag == "obstacle")
+        {
+            movement.enabled = false; //Disables player movement
+
+           
+        }
+    }
+}
