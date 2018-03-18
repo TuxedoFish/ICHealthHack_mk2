e199242511346b10f68cdfdf911f308b2d5cde48
Rhiannon Dowling <r_dowling@live.co.uk>
2018-03-18 13:06:47 +0000

    UI done?

---
+++ Assets/YourScoreFinal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class YourScoreFinal : MonoBehaviour
+{
+    Text finalScore;
+    private void Start()
+    {
+        finalScore = GetComponent<Text>();
+    }
+    void Update()
+    {
+        int AccessingS = ScoreScript.scoreValue;
+        finalScore.text = "Final Score:" + AccessingS;
+
+    }
+}
