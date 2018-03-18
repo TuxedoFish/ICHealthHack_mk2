53c58b869df1cdf81b6c6bc3f066f9e8ab0c5945
Rhiannon Dowling <r_dowling@live.co.uk>
2018-03-18 11:10:06 +0000

    timer countdown

---
+++ Assets/Scripts_Game/TimerCountDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerCountDown : MonoBehaviour
+{
+    public string levelToLoad;
+    private float timer = 10f;
+    private Text timerSeconds;
+
+    //init
+    private void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    //Update
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f2");
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
